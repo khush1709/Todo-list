d6830e3b88a8d221f0fd18e6c37a3fbec9aea07c
khush1709 <83908626+khush1709@users.noreply.github.com>
2023-08-11 17:58:54 +0530

    Update todos.js
    Fixed issue related to adding new tasks

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -11,16 +11,16 @@ $("ul").on("click","span", function(event){
     event.stopPropagation();
 });
 
-$("input[type = 'text'").keypress(function(event){
-    if(event.which == 13){
-        // garbbing new todo text from input
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grabbing new todo text from input
         var todoText = $(this).val();
         // create a new li and add to ul
-        $("ul").append("<li><span><i class='fa-sharp fa-solid fa-trash'></i></span> " + todoText + "</li>");
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
         $(this).val("");
     }
 });
 
 $(".fa-plus").click(function(){
-    $("input[type = 'text'").fadeToggle();
+    $("input[type='text']").fadeToggle();
 });
